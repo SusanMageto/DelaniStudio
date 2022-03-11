62c92afc72f52677c91341fb3d8047884668ed23
SusanMageto <susan.mageto@student.moringaschool.com>
2022-03-12 02:55:19 +0300

    adds the javascript functions for toggling the what we do section

---
+++ script.js
@@ -0,0 +1,18 @@
+$(document).ready (function (){
+  $("#icon1").on ("click", function()
+  {
+    $(".card-body1"). toggle();
+  })
+})
+$(document).ready (function (){
+  $("#icon2").on ("click", function()
+  {
+    $(".card-body2"). toggle();
+  })
+})
+$(document).ready (function (){
+  $("#icon3").on ("click", function()
+  {
+    $(".card-body3"). toggle();
+  })
+})
