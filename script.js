7fa55b1027e6d75cc725cae9081502de23f6f075
SusanMageto <susan.mageto@student.moringaschool.com>
2022-03-14 00:11:39 +0300

    adds slide-up and down to the what we do icons

--- script.js
+++ script.js
@@ -1,21 +1,37 @@
-$(document).ready (function (){
-  $("#icon1").on ("click", function()
-  {
-    $(".card-body1"). toggle();
-  })
-})
-$(document).ready (function (){
-  $("#icon2").on ("click", function()
-  {
-    $(".card-body2"). toggle();
-  })
-})
-$(document).ready (function (){
-  $("#icon3").on ("click", function()
-  {
-    $(".card-body3"). toggle();
-  })
-})
-$('form').submit(function(){
-  alert("Hi "+ $("input[name='name']").val()+ " we have received your message. Thank you for reaching out to us.");
+$(document).ready(function () {
+  $("#card-1").on("click", function () {
+    $("#card-1").slideDown("1000").hide("1000");
+    $("#card-body1").show("1000");
+  });
+});
+$("#card-body1").click(function () {
+  $("#card-body1").slideUp("1000");
+  $("#card-1").slideDown("1000");
+});
+$(document).ready(function () {
+  $("#card-2").on("click", function () {
+    $("#card-2").slideDown("1000").hide("1000");
+    $("#card-body2").show("1000");
+  });
+});
+$("#card-body2").click(function () {
+  $("#card-body2").slideUp("1000");
+  $("#card-2").slideDown("1000");
+});
+$(document).ready(function () {
+  $("#card-3").on("click", function () {
+    $("#card-3").slideDown("1000").hide("1000");
+    $("#card-body3").show("1000");
+  });
+});
+$("#card-body3").click(function () {
+  $("#card-body3").slideUp("1000");
+  $("#card-3").slideDown("1000");
+});
+$("form").submit(function () {
+  alert(
+    "Hi " +
+      $("input[name='name']").val() +
+      " we have received your message. Thank you for reaching out to us."
+  );
 });
