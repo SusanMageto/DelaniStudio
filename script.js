42c5e407966e004315abc0ca32ec02ad57fbc7c5
SusanMageto <susan.mageto@student.moringaschool.com>
2022-03-14 18:50:35 +0300

    styles and fixes bugs in the portfolio part

--- script.js
+++ script.js
@@ -30,13 +30,61 @@ $("#card-body3").click(function () {
 });
 
 $(document).ready(function(){
-  $("#work4").mouseover(function(){
-    $("#exp1").show();
+  $(".one").mouseover(function(){
+    $("#uno1").show();
   }).mouseout(function(){
-    $("#exp1").hide();
+    $("#uno1").hide();
   });
 });
-
+$(document).ready(function(){
+  $(".two").mouseover(function(){
+    $("#uno2").show();
+  }).mouseout(function(){
+    $("#uno2").hide();
+  });
+});
+$(document).ready(function(){
+  $(".three").mouseover(function(){
+    $("#uno3").show();
+  }).mouseout(function(){
+    $("#uno3").hide();
+  });
+});
+$(document).ready(function(){
+  $(".four").mouseover(function(){
+    $("#uno4").show();
+  }).mouseout(function(){
+    $("#uno4").hide();
+  });
+});
+$(document).ready(function(){
+  $(".five").mouseover(function(){
+    $("#uno5").show();
+  }).mouseout(function(){
+    $("#uno5").hide();
+  });
+});
+$(document).ready(function(){
+  $(".six").mouseover(function(){
+    $("#uno6").show();
+  }).mouseout(function(){
+    $("#uno6").hide();
+  });
+});
+$(document).ready(function(){
+  $(".seven").mouseover(function(){
+    $("#uno7").show();
+  }).mouseout(function(){
+    $("#uno7").hide();
+  });
+});
+$(document).ready(function(){
+  $(".eight").mouseover(function(){
+    $("#uno8").show();
+  }).mouseout(function(){
+    $("#uno8").hide();
+  });
+});
 
 $("form").submit(function () {
   alert(
